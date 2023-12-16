8caab6682b66a18da2ef874bc5b47d1e7053e84d
Daniel Szukitsch <daniel@szuuuken.at>
2023-12-16 23:45:34 +0100

    2023 day08 part2 finished

--- 2023/day08/part2/day08_part2.py
+++ 2023/day08/part2/day08_part2.py
@@ -1,4 +1,28 @@
 import re
+from math import lcm
+
+def run_loop(nodes: dict, instructions: str, starting_node_code: (str, str)):
+    instruction_count = len(instructions)
+    searching = True
+    current_node = nodes[starting_node_code]
+    instruction_pointer = 0
+    steps = 0
+    next_node_code = ""
+
+    while searching:
+        steps += 1
+        instruction = instructions[instruction_pointer]
+        next_index = 0 if instruction == 'L' else 1
+        next_node_code = current_node[next_index]
+        
+        current_node = nodes[next_node_code]
+        instruction_pointer += 1
+        if(instruction_pointer >= instruction_count): instruction_pointer = 0
+
+        searching = next_node_code[2] != 'Z' 
+
+    print(f'loop from {starting_node_code} to {next_node_code} took {steps} steps')
+    return steps
 
 def main():
     #file = open('day08_part2_example_input_1.txt', 'r')
@@ -8,9 +32,8 @@ def main():
     lines = file.read().split('\n')
     file.close()
 
-    current_nodes = []
+    starting_nodes = []
     instructions = lines[0]
-    instruction_count = len(instructions)
     nodes = dict()
 
     for line in lines[2:]:
@@ -18,38 +41,11 @@ def main():
         nodes[node_codes[0]] = (node_codes[1], node_codes[2])
 
         if node_codes[0][2] == 'A':
-            current_nodes.append(nodes[node_codes[0]])
+            starting_nodes.append(node_codes[0])
 
-    searching = True
-    instruction_pointer = 0
-    steps = 0
-
-    while searching:
-        steps += 1
-        instruction = instructions[instruction_pointer]
-        next_index = 0 if instruction == 'L' else 1
-
-        next_nodes = []
-        all_end_with_z = True
-        for current_node in current_nodes:
-            next_node = current_node[next_index]
-            next_nodes.append(nodes[next_node])
-
-            if next_node[2] != 'Z':
-                all_end_with_z = False
-
-        searching = not all_end_with_z
-
-        #print(next_nodes)
-        current_nodes = next_nodes
-        
-        instruction_pointer += 1
-        if(instruction_pointer >= instruction_count): instruction_pointer = 0
-
-        if steps % 1000000 == 0:
-            print(steps) 
-
-    print(steps)
+    loops = [run_loop(nodes, instructions, starting_node) for starting_node in starting_nodes]
+    lcm_of_loops = lcm(*loops)
+    print(f'\nthe lcm of the loops is {lcm_of_loops}')
     
 if __name__ == "__main__":
     main()
