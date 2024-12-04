825008cf10ce49f47947020e8809ac61d43cb644
Daniel Szukitsch <daniel.szukitsch@szukitsch.at>
2024-12-04 20:24:28 +0100

    word by word

--- 2024/day04/part01/day04-part01.py
+++ 2024/day04/part01/day04-part01.py
@@ -25,29 +25,34 @@ for y in range(0, len(input)):
     line_len = len(line)
 
     for x in range(0, line_len):
-        print((x,y))
+        print("------")
+        print((x,y), y+3, input_len, y+3 >= input_len)
 
-        left_word = None if x < 3 else input[y][x-3:x]
-        right_word = None if x + 3 >= line_len else input[y][x:x+3] # [0,1,2,3,4,5,6,7,8,9] --> len == 10
-        top_word = None if y < 3 else input[y-3:y][x]
+        left_word = None if x < 3 else input[y][x-3:x+1]
+        right_word = None if x + 3 > line_len else input[y][x:x+4] # [0,1,2,3,4,5,6,7,8,9] --> len == 10
+        top_word = None if y < 3 else ''.join([l[x] for l in input[y-3:y+1]])
+        bottom_word = None if y + 3 >= input_len else ''.join([l[x] for l in input[y:y+4]])
+        left_bottom_word = None if y > 2 and x > 2 else input[y-3][x-3] + input[y-2][x-2] + input[y-1][x-1] + input[y][x]
+        left_top_word = None if y + 3 >= input_len and x > 2 else input[y+3][x-3] + input[y+2][x-2] + input[y+1][x-1] + input[y][x]
 
         # print(input_len)
-        # print(input[y])
+        #print(input[y])
         # print(input[y+1])
         # print(input[y+2])
-        p# rint(input[y+3])
-        bottom_word = None if y + 3 >= input_len else input[y:y+3][x]
-
-    print("------")
-    print(left_word)
-    print(right_word)
-    print(top_word)
-    print(bottom_word)
-    print("-------")
+        # print(input[y+3])
 
 
 
---pp(input)
+        #print(left_word)
+        #print(right_word)
+        #print(top_word)
+        #print(bottom_word)
+        print(left_bottom_word)
+        #print("-------")
+
+
+
+# pp(input)
 exit()
 
 commands = re.findall(r'mul\([1-9]{1}[0-9]{0,2},[1-9]{1}[0-9]{0,2}\)', input)
